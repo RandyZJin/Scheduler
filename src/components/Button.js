9a2ff8037143195dc69def04e06a256fb4afbdff
RJ <randy.j416@yahoo.com>
2023-02-21 15:28:36 -0500

    tested buttons with classNames

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+
+   // classNames("button", {confirm: false}, {danger:false}, {disabled:false}, {onClick: true})
+   // if (props.confirm) {
+   //   buttonClass += " button--confirm";
+   // }
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   //  }
+    return (
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    );
 }
